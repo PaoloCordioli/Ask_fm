0a76dd19e15db43c60d259f58cda3f02d0fb53d2
PaoloCordioli <p.cordioli@hotmail.it>
2020-04-26 16:59:46 +0200

    Add order by date desc

--- Backend/Questions/src/utils/mongoDB.ts
+++ Backend/Questions/src/utils/mongoDB.ts
@@ -9,12 +9,12 @@ export class MongoDB {
     private constructor() {
     }
 
-    private async init(url: string, db_name: string, db_collection: string) : Promise<void> {
+    private async init(url: string, db_name: string, db_collection: string): Promise<void> {
         const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
         this.collection = client.db(db_name).collection(db_collection)
     }
 
-    public static async get_instance() : Promise<MongoDB> {
+    public static async get_instance(): Promise<MongoDB> {
         if (!MongoDB.instance) {
             MongoDB.instance = new MongoDB()
             await MongoDB.instance.init(process.env.MONGO_URI, "Ask", "Questions")
@@ -23,7 +23,7 @@ export class MongoDB {
     }
 
     public async get(): Promise<Array<any>> {
-        const questions = await this.collection.find().toArray()
+        const questions = await this.collection.find().sort({ date: -1 }).toArray()
         return questions
     }
 
@@ -31,13 +31,13 @@ export class MongoDB {
         this.collection.insertOne(question)
     }
 
-    public async get_questions_by_username(username : string): Promise<any> {
-        const questions = await this.collection.find({ username : username }).toArray()
+    public async get_questions_by_username(username: string): Promise<any> {
+        const questions = await this.collection.find({ username: username }).sort({ date: -1 }).toArray()
         return questions
     }
 
-    public update (id : string, answer : string) : void {
+    public update(id: string, answer: string): void {
         const _id = new ObjectID(id)
-        this.collection.updateOne({_id : _id }, {$set : {answer : answer}})
+        this.collection.updateOne({ _id: _id }, { $set: { answer: answer } })
     }
 }
